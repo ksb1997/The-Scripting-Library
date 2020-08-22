458a06f90ba9948c5643019d4d5aeba11c9a1662
ksb1997 <44491299+ksb1997@users.noreply.github.com>
2020-08-22 19:03:07 +0530

    Powershell Scripts
    This is an Powershell Script for port scanning.

---
+++ portscanning.ps1
@@ -0,0 +1,7 @@
+﻿ $name=Read-Host "Target"
+ $port=Read-Host "Port Numbers"
+ $port.split(',') | Foreach-Object -Process {If (($a=Test-NetConnection $name -Port $_ -WarningAction SilentlyContinue).tcpTestSucceeded -eq $true) 
+ {Write-Host $a.Computername -ForegroundColor Green " ==> " $a.RemotePort " : " " Open"} 
+ else 
+ {Write-Host $a.Computername -ForegroundColor Red   " ==> " $a.RemotePort " : " "Closed"}
+ }
